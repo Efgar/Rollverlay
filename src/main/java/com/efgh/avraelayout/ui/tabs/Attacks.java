91efa5e0729b6c35d47e5b08e9c06452d83ca474
Edgar Garcia <edgar.hernandez@ubs.com>
2019-04-05 12:51:24 +0200

    Generating executable files

--- src/main/java/com/efgh/avraelayout/ui/tabs/Attacks.java
+++ src/main/java/com/efgh/avraelayout/ui/tabs/Attacks.java
@@ -1,10 +1,19 @@
 package com.efgh.avraelayout.ui.tabs;
 
 import javafx.scene.control.Tab;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 public class Attacks extends Tab {
 
     public Attacks() {
         setText("Attacks");
+
+        HBox expressionButtons = new HBox();
+        expressionButtons.getStyleClass().add("hbox");
+        expressionButtons.setPrefHeight(Double.MAX_VALUE);
+
+        expressionButtons.getChildren().add(new Text("Coming soon..."));
+        setContent(expressionButtons);
     }
 }
--- src/main/java/com/efgh/avraelayout/ui/tabs/Spells.java
+++ src/main/java/com/efgh/avraelayout/ui/tabs/Spells.java
@@ -1,10 +1,19 @@
 package com.efgh.avraelayout.ui.tabs;
 
 import javafx.scene.control.Tab;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 public class Spells extends Tab {
 
     public Spells() {
         setText("Spells");
+
+        HBox expressionButtons = new HBox();
+        expressionButtons.getStyleClass().add("hbox");
+        expressionButtons.setPrefHeight(Double.MAX_VALUE);
+
+        expressionButtons.getChildren().add(new Text("Coming soon..."));
+        setContent(expressionButtons);
     }
 }
